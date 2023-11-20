6750914fc0c8e20dd00d6c1b0622572318af932f
Olvenn <olga-vencel@yandex.ru>
2023-11-20 15:42:02 +0300

    Add 2 projects.

---
+++ Examples/Example001HelloConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Hello, World!");
+
+string username = Console.ReadLine();
+
+Console.WriteLine("Hello," + username);
+
+// Выводит в одну строку
+Console.Write("Hello, World!");
+
+string username2 = Console.ReadLine();
+
+Console.Write("Hello," + username);
---
+++ Examples/Example003If/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите имя пользователя:  ");
+string username = Console.ReadLine();
+Console.WriteLine(username);
+
+if(username == "olga")
+{
+  Console.WriteLine("Hi, Olga!");
+}
+else 
+{
+  Console.Write("Hello, ");
+  Console.WriteLine(username);
+}
+// Console.WriteLine("Hello, World!");
