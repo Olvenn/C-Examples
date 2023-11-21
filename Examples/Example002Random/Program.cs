82a276cef4930e7c0912d3148d62cc569bb99d9d
Olvenn <olga-vencel@yandex.ru>
2023-11-21 15:29:32 +0300

    Add 2 if and while.

--- Examples/Example002Random/Program.cs
+++ Examples/Example002Random/Program.cs
@@ -4,3 +4,20 @@ int numberB = new Random().Next(1, 10); // 1, 2, 3 ... 9
 Console.WriteLine(numberB);
 int result = numberA |+ numberB;
 Console.WriteLine(result);
+
+double numberADouble = 12;
+double numberBDouble = 5;
+
+Console.WriteLine(numberADouble / numberBDouble);
+
+// Найти мах из 4 чисел
+
+int a = 2;
+int b = 5;
+int c = 3;
+int d = 1;
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+Console.WriteLine("max = " + max);
