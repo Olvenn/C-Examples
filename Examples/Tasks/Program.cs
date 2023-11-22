b0836ee16289084afae9eb624fce8ac302b4a825
Olvenn <olga-vencel@yandex.ru>
2023-11-22 16:34:21 +0300

    Add task1.

---
+++ Examples/Tasks/Program.cs
@@ -0,0 +1,14 @@
+﻿// Console.WriteLine("Hello, World!");
+// int num = 456;
+// int first = num / 100;
+// int second = num % 10;
+// int third = num % 100;
+// Console.WriteLine($"{first} {second} {third}");
+// double middle = (num % 100 - num % 10) / 10;
+// double last = num % 10;
+
+// double pow = Math.Pow(middle, last);
+
+// Console.WriteLine($"{pow}");
+
+
