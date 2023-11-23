739c84166a6b0694c75f619c0edfc0da5aaf128e
Olvenn <olga-vencel@yandex.ru>
2023-11-23 15:55:06 +0300

    Task from seminar.

--- Examples/Tasks/Program.cs
+++ Examples/Tasks/Program.cs
@@ -11,4 +11,33 @@
 
 // Console.WriteLine($"{pow}");
 
+// На вход два числа. Является ли второе число кратным первому.
 
+// int first = 156;
+// int second = 11;
+
+// if (first % second == 0) {
+//     Console.WriteLine("да");
+// } else {
+//     Console.WriteLine($"нет, {first % second}");
+// }
+
+// На вход число. Если есть третья цифра выводим (7812 -> 1), если нет Третьей цифры нет.
+
+int num = 114561;
+if (num < 0) {
+  num *= (-1);
+}
+if (num / 100 < 0) {
+   Console.WriteLine("Третьей цифры нет");
+} else if (num / 100 < 10) {
+     Console.WriteLine($"{num % 10}");
+} else {
+  while (num / 100 > 10) { // (num > 999)
+    num /= 10;
+  }
+  Console.WriteLine($"{num % 10}");
+}
+
+        // string str = num.ToString();
+        // Console.WriteLine(str[1]);
